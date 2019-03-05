6bf4aa3825815ffd0e45508e7888095e6067fc45
Ripster47 <mpolinski12@gmail.com>
2019-03-05 17:01:12 -0600

    users controller/model created

---
+++ db/migrate/20190305224446_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+      t.string :phone_number
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
