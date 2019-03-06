b5c0f39a49e5a50b004ed923494579d850ec578a
Ripster47 <mpolinski12@gmail.com>
2019-03-06 17:10:59 -0600

    added associations, need to test all CRUD routes, actions, views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+  belongs_to :animals
+  has_many :submissions
+  has_many :animals, through: :submissions
+
   validates :email, presence: true, uniqueness: true
+
+
 end
