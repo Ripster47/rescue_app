b3960451aa6bae2beb2df10c2611adb1bca153b6
Ripster47 <mpolinski12@gmail.com>
2019-03-13 14:06:21 -0500

    added enum to accommodatins

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,7 +17,20 @@ class Api::UsersController < ApplicationController
                     address_2: params[:address_2],
                     city: params[:city],
                     state: params[:state],
-                    zip_code: params[:zip_code]
+                    zip_code: params[:zip_code],
+                    rent: params[:rent],
+                    children: params[:children],
+                    adult: params[:adult],
+                    volunteer: params[:volunteer],
+                    foster: params[:foster],
+                    usark: params[:usark],
+                    exotic_vet: params[:exotic_vet],
+                    ever_owned: params[:ever_owned],
+                    care: params[:care],
+                    provisions: params[:provisions],
+                    bequest: params[:bequest],
+                    accommodations: params[:accommodations],
+                    permit: params[:permit]
                     )
     if user.save
       render json: {message: 'User created successfully'}, status: :created
@@ -45,6 +58,19 @@ class Api::UsersController < ApplicationController
     @user.city = params[:city] || @user.city
     @user.state = params[:state] || @user.state
     @user.zip_code = params[:zip_code] || @user.zip_code
+    @user.rent = params[:rent] || @user.rent
+    @user.children = params[:children] || @user.children
+    @user.adult = params[:adult] || @user.adult
+    @user.volunteer = params[:volunteer] || @user.volunteer
+    @user.foster = params[:foster] || @user.foster
+    @user.usark = params[:usark] || @user.usark
+    @user.exotic_vet = params[:exotic_vet] || @user.exotic_vet
+    @user.ever_owned = params[:ever_owned] || @user.ever_owned
+    @user.care = params[:care] || @user.care
+    @user.provisions = params[:provisions] || @user.provisions
+    @user.bequest = params[:bequest] || @user.bequest
+    @user.accommodations = params[:accommodations] || @user.accommodations
+    @user.permit = params[:permit] || @user.permit
 
     if @user.save
       render 'show.json.jbuilder'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
 
+  enum accommodations: { Apartment: 1, Condominium: 2, House: 3, Other: 4}
+
 
 end
